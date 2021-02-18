770aa091616419c9e0bfa59945c463bd462c4ebf
Amit833 <amitmajumder833@gmail.com>
2021-02-18 13:52:32 +0100

    form-css

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,32 +1,45 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { addItem, decreaseItem, deleteItem, emptyBasket } from '../actions/basketActions';
+import {
+  addItem,
+  decreaseItem,
+  deleteItem,
+  emptyBasket,
+} from "../actions/basketActions";
 
 export const Basket = () => {
-    const state = useSelector(state => state.takeawayReducer.items);
-    const dispatch = useDispatch()
-    const subTotal = state
+  const state = useSelector((state) => state.takeawayReducer.items);
+  const dispatch = useDispatch();
+  const subTotal = state
     .reduce((acc, item) => acc + item.price.price * item.qty, 0)
     .toFixed(2);
-    const basketItem = state.map((item) => {
-        return (
-            <div key={item.id}>
-                <button onClick={() => dispatch(deleteItem(item.id))}>x</button>
-                <h2>{item.name}</h2>
-                <p>{item.price.price * item.qty}</p>
-                <button onClick={() => dispatch(decreaseItem(item))}>-</button>
-                <p>{item.qty}</p>
-                <button onClick={() => dispatch(addItem(item))}>+</button>
-            </div>
-        )
-    })
+  const basketItem = state.map((item) => {
     return (
-        <>
-            {state.length === 0 ? <h2>Basket is empty</h2> : 
-            <div>{basketItem}<div>{subTotal}</div></div>}
-            <button onClick={() => dispatch(emptyBasket())}>Empty Basket</button>
-            <Link to='/checkout'><button>Buy Now</button></Link>
-        </>
-        )
-}
+      <div key={item.id}>
+        <button onClick={() => dispatch(deleteItem(item.id))}>x</button>
+        <h2>{item.name}</h2>
+        <p>{item.price.price * item.qty}</p>
+        <button onClick={() => dispatch(decreaseItem(item))}>-</button>
+        <p>{item.qty}</p>
+        <button onClick={() => dispatch(addItem(item))}>+</button>
+      </div>
+    );
+  });
+  return (
+    <>
+      {state.length === 0 ? (
+        <h2>Basket is empty</h2>
+      ) : (
+        <div>
+          {basketItem}
+          <div>{subTotal}</div>
+        </div>
+      )}
+      <button onClick={() => dispatch(emptyBasket())}>Empty Basket</button>
+      <Link to="/checkout">
+        <button>Buy Now</button>
+      </Link>
+    </>
+  );
+};
