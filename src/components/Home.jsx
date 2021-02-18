de795a6934dd7ecbe395fb1f37f60700b68a16ae
Elias Guderian <elias-guderian@outlook.com>
2021-02-18 17:04:55 +0100

    style Nav

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { Header } from './Header';
 export const Home = () => {
     return (
         <div>
-            <Header/>
+        
         </div>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom';
 
 export const Nav = () => {
+    const [mobileToggle, setMobileToggle] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const breakpointM = 786;
+
+    const hamburgerLogic = () => {
+        if (mobileToggle === true) {
+            return (<div className='nav-links'>
+                <div className='nav-link'>
+                    <NavLink to='/takeawaymenu' >Take Away</NavLink>
+                </div>
+                <div className='nav-link'>
+                    <NavLink to='/signup' >Sign Up</NavLink>
+                </div>
+                <div className='nav-link'>
+                    <NavLink to='/signin' >Sign In</NavLink>
+                </div>
+            </div>)
+        }
+    }
+
+
+    window.addEventListener('resize', () => {
+        return setWindowWidth(window.innerWidth)
+    })
+    console.log(windowWidth)
+
     return (
-        <div>
-            <NavLink to='/basket' >basket</NavLink>
-            <NavLink to='/takeawaymenu' >Take Away</NavLink>
-            <NavLink to='/signup' >Sign Up</NavLink>
-            <NavLink to='/signin' >Sign In</NavLink>
+        <div className='nav'>
+            {windowWidth > breakpointM ? <div className='nav-links'>
+                <div className='nav-link'>
+                    <NavLink to='/takeawaymenu' >Take Away</NavLink>
+                </div>
+                <div className='nav-link'>
+                    <NavLink to='/signup' >Sign Up</NavLink>
+                </div>
+                <div className='nav-link'>
+                    <NavLink to='/signin' >Sign In</NavLink>
+                </div>
+            </div> : <div>
+                    <div className='hamburgerToggle' onClick={() => setMobileToggle(!mobileToggle)}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    {hamburgerLogic()}
+                </div>
+            }
+            <div className='basket-icon'>
+                <div className='nav-link'>
+                    <NavLink to='/basket' >basket</NavLink>
+                </div>
+            </div>
         </div>
     )
 }
+
+
+
+
+// { mobileToggle && windowWidth <= breakpointM && <div className='nav-links'>
+//                 <div className='nav-link'>
+//                     <NavLink to='/takeawaymenu' >Take Away</NavLink>
+//                 </div>
+//                 <div className='nav-link'>
+//                     <NavLink to='/signup' >Sign Up</NavLink>
+//                 </div>
+//                 <div className='nav-link'>
+//                     <NavLink to='/signin' >Sign In</NavLink>
+//                 </div>
+//             </div>}
