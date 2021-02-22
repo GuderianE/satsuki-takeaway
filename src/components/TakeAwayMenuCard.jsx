7bf7ac40be8f51f10201018a23f61ea4ac1c0f6f
carveler <3heyaberlin@gmail.com>
2021-02-22 14:14:42 +0100

    still working menu

--- src/components/TakeAwayMenuCard.jsx
+++ src/components/TakeAwayMenuCard.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { addItem } from '../actions/basketActions';
 
-export const TakeAwayMenuCard = ({item}) => {
-    const dispatch = useDispatch();
-    return (
-        <div>
-            <img src={item.images} alt="Lorem Picsum"/>
-            <div>{item.name}</div>
-            <div>{item.price.price}</div>
-            <button onClick={() => dispatch(addItem(item))}>add to basket</button>
-        </div>
-    );
+export const TakeAwayMenuCard = ({ item }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className='menu_card'>
+      <img src={item.images} alt='Lorem Picsum' />
+      <div>
+        <strong>{item.name}</strong>
+      </div>
+      <div>€{item.price.price}</div>
+      <button onClick={() => dispatch(addItem(item))}>add to basket</button>
+    </div>
+  );
 };
